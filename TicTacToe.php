547e31d85127e1036be68ab0d0957540dafb02be
Aramur <benjaminnater08@gmail.com>
2022-04-01 21:55:38 +0200

    design website

---
+++ TicTacToe.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>TicTacToe</title>
+
+    <style>
+        body{
+            background-color: black;
+            text-align: center;
+        }
+        table{
+            margin: auto;
+            border-collapse: collapse;
+            color: darkred;
+        }
+        td{
+            border: 1px solid darkred;
+            width: 100px;
+            height: 100px;
+            text-align: center;
+        }
+        img{
+         height: 75px;
+         width: 75px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1><span style="color:darkred">Glarotech Tic-Tac-Toe</span></h1>
+    <table>
+        <tr>
+            <td>>1</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+        </tr>
+    </table>
+
+    <br>
+
+   <button type="button" name= "button1">1</button>
+   <button type="button" name= "button2">2</button>
+   <button type="button" name= "button3">3</button>
+   <button type="button" name= "button4">4</button>
+   <button type="button" name= "button5">5</button>
+   <button type="button" name= "button6">6</button>
+   <button type="button" name= "button7">7</button>
+   <button type="button" name= "button8">8</button>
+   <button type="button" name= "button9">9</button>
+
+</body>
