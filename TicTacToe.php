fedf67244112f219e8baee0f95a158a049caf73f
Aramur <benjaminnater08@gmail.com>
2022-04-06 22:46:24 +0200

    the winner is

--- TicTacToe.php
+++ TicTacToe.php
@@ -32,11 +32,14 @@
     <?php
         session_start();
 
-        $WerIstDran = 0;
-        if(isset($_SESSION['werIstDran'])){
-            $WerIstDran = $_SESSION['werIstDran'];
+        if (isset($_POST['reset'])){
+            sessionLöschen();
         }
-        $number;
+
+        $gewonnen = 0;
+        if(isset($_SESSION['gewonnen'])){
+            $gewonnen = $_SESSION['gewonnen'];
+        }
         $feldbesetzung = [
             1 => 'leer',
             2 => 'leer',
@@ -48,37 +51,39 @@
             8 => 'leer',
             9 => 'leer'
         ];
+
         if(isset($_SESSION['feldbesetzung'])){
             $feldbesetzung = $_SESSION['feldbesetzung'];
         }
 
-        $lösungen = [
-            123,
-            456,
-            789,
-            147,
-            258,
-            369,
-            159,
-            357,
-        ];
-
-        if(!empty($_POST)){
-            if (isset($_POST['button'])){
-                $number =  $_POST['button'];
-
-                welchessymbol($number, $feldbesetzung, $WerIstDran);
-                $_SESSION['werIstDran'] = $WerIstDran;
-                $_SESSION['feldbesetzung'] = $feldbesetzung;
+        if($gewonnen == 0);{
+            $WerIstDran = 0;
+            if(isset($_SESSION['werIstDran'])){
+                $WerIstDran = $_SESSION['werIstDran'];
             }
-            if (isset($_POST['reset'])){
-                sessionLöschen();
+
+            if(!empty($_POST)){
+                if (isset($_POST['button'])){
+                    $number =  $_POST['button'];
+
+                    welchessymbol($number, $feldbesetzung, $WerIstDran);
+                    $_SESSION['werIstDran'] = $WerIstDran;
+                    $_SESSION['feldbesetzung'] = $feldbesetzung;
+
+                    werHatGewonnen('Glarotechsymbol', $gewonnen, $feldbesetzung);
+                    werHatGewonnen('Peppershopsymbol', $gewonnen, $feldbesetzung);
+                }
             }
+
+            $_SESSION['gewonnen'] = $gewonnen;
         }
 
-
-
-
+        if($gewonnen == 1){
+            echo 'Glarotech hat gewonnen!';
+        }
+        elseif($gewonnen == 2){
+            echo 'Peppershopsymbol hat gewonnen!';
+        }
 
         function welchessymbol($number, &$feldbesetzung, &$WerIstDran){
             if($feldbesetzung[$number] == 'leer'){
@@ -96,6 +101,7 @@
         function sessionLöschen(){
             $_SESSION['werIstDran'] = NULL;
             $_SESSION['feldbesetzung'] = NULL;
+            $_SESSION['gewonnen'] = NULL;
         }
 
         function symboleanzeigen($feld, $feldbesetzung){
@@ -103,14 +109,34 @@
                 echo $feld;
             }
             elseif($feldbesetzung[$feld] == 'Peppershopsymbol'){
-               echo '<img src="images/peppershop_logo_hires.png">';
+            echo '<img src="images/peppershop_logo_hires.png">';
             }
             elseif($feldbesetzung[$feld] == 'Glarotechsymbol'){
-                 echo '<img src="images/glarotech_weiss_blau.png">';
+                echo '<img src="images/glarotech_weiss_blau.png">';
             }
         }
 
+        function werHatGewonnen($name, &$gewonnen, $feldbesetzung){
+            welcheLösung($name, 1, 2, 3, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 4, 5, 6, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 7, 8, 9, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 1, 4, 7, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 2, 5, 8, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 3, 6, 9, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 1, 5, 9, $gewonnen, $feldbesetzung);
+            welcheLösung($name, 3, 5, 7, $gewonnen, $feldbesetzung);
+        }
+
+        function welcheLösung($name, $number1, $number2, $number3, &$gewonnen, $feldbesetzung){
+            if($name == 'Glarotechsymbol' && $feldbesetzung[$number1] == 'Glarotechsymbol' && $feldbesetzung[$number2] == 'Glarotechsymbol' && $feldbesetzung[$number3] == 'Glarotechsymbol'){
+                $gewonnen = 1;
+            }
+            elseif($name == 'Peppershopsymbol' && $feldbesetzung[$number1] == 'Peppershopsymbol' && $feldbesetzung[$number2] == 'Peppershopsymbol' && $feldbesetzung[$number3] == 'Peppershopsymbol'){
+                $gewonnen = 2;
+            }
+        }
     ?>
+
 
     <h1><span style="color:darkred">Glarotech Tic-Tac-Toe</span></h1>
     <table>
@@ -142,7 +168,6 @@
         <button type="sumbit" name= "button" value= "7">7</button>
         <button type="sumbit" name= "button" value= "8">8</button>
         <button type="sumbit" name= "button" value= "9">9</button>
-        <br>
         <button type="sumbit" name= "reset">Neustart</button>
     </form>
 </body>
